1743b82012ad4b6807729650174cd40971b6583f
caisilus <serangor@gmail.com>
2020-11-28 19:50:58 +0300

    initial commit

---
+++ Sbornik_Bot/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sbornik_Bot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
